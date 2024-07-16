8fd38021bb9888a7c9e4ffe7a5121530067202fd
aneins <zengxin.jiao@tum.de>
2024-07-17 02:59:54 +0800

    json文件

--- example_scripts.sh
+++ example_scripts.sh
@@ -1,5 +1,17 @@
-python run_nerf.py --config configs/lego_active.txt --expname active_lego --basedir ./logs --datadir ./data/lego
+python run_nerf.py --config configs/chair_active.txt --expname chair --basedir ./logs --datadir ./data/blender/chair
 
-python run_nerf.py --config configs/hotdog_active.txt --expname active_hotdog --basedir ./logs --datadir ./data/hotdog
+python run_nerf.py --config configs/mic_active.txt --expname mic --basedir ./logs --datadir ./data/blender/mic
 
-python run_nerf.py --config configs/llff_active.txt --expname active_fern --basedir ./logs --datadir ./data/fern  --w 0.0 --init_image 5 --choose_k 2
+python run_nerf.py --config configs/ficus_active.txt --expname ficus --basedir ./logs --datadir ./data/blender/ficus
+
+
+python run_nerf.py --config configs/hotdog_active.txt --expname hotdog --basedir ./logs --datadir ./data/blender/hotdog
+
+python run_nerf.py --config configs/materials_active.txt --expname materials --basedir ./logs --datadir ./data/blender/materials
+
+python run_nerf.py --config configs/drums_active.txt --expname drums --basedir ./logs --datadir ./data/blender/drums
+
+python run_nerf.py --config configs/ship_active.txt --expname ship --basedir ./logs --datadir ./data/blender/ship
+
+python run_nerf.py --config configs/lego_active.txt --expname lego --basedir ./logs --datadir ./data/blender/lego
+
